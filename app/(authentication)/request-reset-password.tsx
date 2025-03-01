263719ef6cc353970e8f9d689a7be58d4f9fc05d
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-01 21:57:03 +0300

    feat(authentication): add request reset password screen

---
+++ app/(authentication)/request-reset-password.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import RequestResetPasswordScreen from '@/src/feature/authentication/presentation/RequestResetPasswordScreen';
+
+const RequestResetPassword = () => {
+  return <RequestResetPasswordScreen/>
+};
+export default RequestResetPassword;
--- src/feature/authentication/presentation/RequestResetPasswordScreen.tsx
+++ src/feature/authentication/presentation/RequestResetPasswordScreen.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 import { router } from 'expo-router';
 import { ArrowLeft } from 'lucide-react-native';
 import FieldInput from '@/src/shared/presentation/form/field-input';
-import PasswordFieldInput from '@/src/shared/presentation/form/password-input';
 import Button from '@/src/shared/presentation/form/button';
 
-const SignInScreen = () => {
+const RequestResetPasswordScreen = () => {
   return (
     <SafeAreaView className="bg-primary h-screen w-screen">
       <View className="flex flex-col justify-between items-center h-full w-full">
@@ -16,27 +15,15 @@ const SignInScreen = () => {
           </TouchableOpacity>
           <View className="flex flex-col justify-start items-center w-full my-[24px] gap-y-[32px]">
             <View className="gap-y-[16px] flex flex-col justify-start items-start w-full">
-              <Text className="font-urbanist-bold text-[32px] text-white">Welcome back 👋</Text>
-              <Text className="font-urbanist-regular text-[18px] text-white">Please enter your email & password to log in.</Text>
+              <Text className="font-urbanist-bold text-[32px] text-white">Reset your password 🔑</Text>
+              <Text className="font-urbanist-regular text-[18px] text-white">
+                Please enter your email and we will send an OTP code in the next step to reset your password.
+              </Text>
             </View>
           </View>
           <View className="flex flex-col justify-start items-center w-full my-[24px] gap-y-[24px]">
             <FieldInput type="email" label="Email" placeholder="Enter your email" value="" onChange={(value) => console.log(value)} />
-            <PasswordFieldInput label="Password" placeholder="Enter your password" value="" onChange={(value) => console.log(value)} />
-            <View className="flex flex-row justify-start items-center w-full gap-x-[8px] mt-7">
-              <View className="flex justify-center items-center w-full">
-                <TouchableOpacity onPress={() => router.push('/request-reset-password')}>
-                  <Text className="text-secondary font-urbanist-bold text-[16px]">Forgot password?</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
           </View>
-        </View>
-        <View className="w-full py-[20px] flex flex-row justify-center items-center">
-          <Text className="text-white font-urbanist-medium text-[16px]">Don't have an account?</Text>
-          <TouchableOpacity onPress={() => router.push('/sign-up')}>
-            <Text className="text-secondary font-urbanist-bold text-[16px] ml-[8px]">Sign up</Text>
-          </TouchableOpacity>
         </View>
         <View className="w-screen p-[24px] border-t-[1px] border-t-quaternary">
           <Button onPress={() => router.push('/sign-up')} label="Continue" type="primary" />
@@ -45,4 +32,4 @@ const SignInScreen = () => {
     </SafeAreaView>
   );
 };
-export default SignInScreen;
+export default RequestResetPasswordScreen;
