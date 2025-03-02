54a2c9686941491b6327a12f588fa6e9aa0de562
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-02 08:10:13 +0300

    feat: add authentication repository

---
+++ src/feature/authentication/domain/repository/auth-repository.ts
@@ -0,0 +1,12 @@
+import {Either} from "fp-ts/Either";
+import { Failure } from '@/src/core/error/failure';
+import { Auth } from '@/src/feature/authentication/domain/entity/auth';
+import { GeneralResponse } from '@/src/shared/domain/entity/general-response';
+
+export interface AuthRepository {
+  signIn(email: string, password: string): Promise<Either<Failure, Auth>>;
+  signUp(name: string, email: string, password: string): Promise<Either<Failure, Auth>>;
+  requestResetPassword(email: string): Promise<Either<Failure, GeneralResponse>>;
+  otp(email: string, otp: string): Promise<Either<Failure, GeneralResponse>>;
+  resetPassword(email: string, password: string): Promise<Either<Failure, GeneralResponse>>;
+}
