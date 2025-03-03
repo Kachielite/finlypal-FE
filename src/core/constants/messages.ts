be9220dc29f47ba70a95edf6f489fcd3541aac05
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-03 07:53:43 +0300

    feat(authentication): add auth handler functions in authBloc's handleAuthEvent method

--- src/core/constants/messages.ts
+++ src/core/constants/messages.ts
@@ -1,9 +1,15 @@
 const ERROR = "An unknown error has occurred";
 const SIGN_IN_SUCCESS = "Welcome back";
 const SIGN_UP_SUCCESS = "Welcome to the FinlyPal App";
+const RESET_PASSWORD_SUCCESS = "Password reset successful. Now you can sign in with your new password";
+const VERIFY_OTP_SUCCESS = "OTP verification successful";
+const REQUEST_RESET_PASSWORD_SUCCESS = "Reset password link sent to your email";
 
 export default {
     ERROR,
     SIGN_IN_SUCCESS,
-    SIGN_UP_SUCCESS
+    SIGN_UP_SUCCESS,
+    RESET_PASSWORD_SUCCESS,
+    VERIFY_OTP_SUCCESS,
+    REQUEST_RESET_PASSWORD_SUCCESS
 }
--- src/feature/authentication/presentation/state/authBloc.ts
+++ src/feature/authentication/presentation/state/authBloc.ts
@@ -1,12 +1,26 @@
 import { useAuthState } from './authState';
 import { AUTH_EVENTS } from './authEvent';
-import { signUpUseCase } from '@/src/init_dependencies';
+import {
+  requestResetPasswordUseCase,
+  resetPasswordUseCase,
+  signInUseCase,
+  signUpUseCase,
+  verifyOtpUseCase
+} from '@/src/init_dependencies';
 import { fold } from 'fp-ts/Either';
 import { Failure } from '@/src/core/error/failure';
 import { Auth } from '@/src/feature/authentication/domain/entity/auth';
 import { router } from 'expo-router';
 import { showToast } from '@/src/shared/presentation/components/toastProvider';
 import messages from '@/src/core/constants/messages';
+import { SignInUseCaseParams } from '@/src/feature/authentication/domain/use-case/use-sign-in';
+import { SignUpUseCaseParam } from '@/src/feature/authentication/domain/use-case/use-sign-up';
+import {
+  RequestResetPasswordUseCaseParams
+} from '@/src/feature/authentication/domain/use-case/use-request-reset-password';
+import { GeneralResponse } from '@/src/shared/domain/entity/general-response';
+import { VerifyOtpUseCaseParams } from '@/src/feature/authentication/domain/use-case/use-verify-otp';
+import { ResetPasswordUseCaseParams } from '@/src/feature/authentication/domain/use-case/use-reset-password';
 
 export const authBloc = {
   handleAuthEvent: async (event: string, payload?: any) => {
@@ -15,6 +29,18 @@ export const authBloc = {
       case AUTH_EVENTS.SIGN_UP:
         await signUpHandler(payload, useAuthState.getState);
         break;
+      case AUTH_EVENTS.SIGN_IN:
+        await signInHandler(payload, useAuthState.getState);
+        break;
+      case AUTH_EVENTS.REQUEST_RESET_PASSWORD:
+        await requestResetPasswordHandler(payload, useAuthState.getState);
+        break;
+      case AUTH_EVENTS.VERIFY_OTP:
+        await verifyOtpHandler(payload, useAuthState.getState);
+        break;
+      case AUTH_EVENTS.RESET_PASSWORD:
+        await resetPasswordHandler(payload, useAuthState.getState);
+        break;
       default:
         break;
     }
@@ -22,11 +48,10 @@ export const authBloc = {
 };
 
 
-const signUpHandler = async (payload: {name: string, email: string, password: string}, getState: typeof useAuthState.getState) => {
+const signUpHandler = async (payload: SignUpUseCaseParam, getState: typeof useAuthState.getState) => {
   const {setToken, setIsLoading} = getState();
 
   setIsLoading(true);
-
   const response = await signUpUseCase.execute(payload);
 
   fold<Failure, Auth, void>(
@@ -39,6 +64,88 @@ const signUpHandler = async (payload: {name: string, email: string, password: st
       setIsLoading(false);
       router.push("/")
       showToast('error', 'Success!', messages.SIGN_UP_SUCCESS)
+    }
+  )(response)
+
+}
+
+const signInHandler = async (payload: SignInUseCaseParams, getState: typeof useAuthState.getState) => {
+  const {setToken, setIsLoading} = getState();
+
+  setIsLoading(true);
+  const response = await signInUseCase.execute(payload);
+
+  fold<Failure, Auth, void>(
+    (failure) => {
+      setIsLoading(false);
+      showToast('error', 'Error!', failure.message || messages.ERROR)
+    },
+    (auth) => {
+      setToken(auth);
+      setIsLoading(false);
+      router.push("/")
+      showToast('error', 'Success!', messages.SIGN_IN_SUCCESS)
+    }
+  )(response)
+
+}
+
+const requestResetPasswordHandler = async (payload: RequestResetPasswordUseCaseParams, getState: typeof useAuthState.getState) => {
+  const {setIsLoading} = getState();
+
+  setIsLoading(true);
+  const response = await requestResetPasswordUseCase.execute(payload);
+
+  fold<Failure, GeneralResponse, void>(
+    (failure) => {
+      setIsLoading(false);
+      showToast('error', 'Error!', failure.message || messages.ERROR)
+    },
+    () => {
+      setIsLoading(false);
+      router.push("/otp")
+      showToast('error', 'Success!', messages.REQUEST_RESET_PASSWORD_SUCCESS)
+    }
+  )(response)
+
+}
+
+
+const verifyOtpHandler = async (payload: VerifyOtpUseCaseParams, getState: typeof useAuthState.getState) => {
+  const {setIsLoading} = getState();
+
+  setIsLoading(true);
+  const response = await verifyOtpUseCase.execute(payload);
+
+  fold<Failure, GeneralResponse, void>(
+    (failure) => {
+      setIsLoading(false);
+      showToast('error', 'Error!', failure.message || messages.ERROR)
+    },
+    () => {
+      setIsLoading(false);
+      router.push("/reset-password")
+      showToast('error', 'Success!', messages.VERIFY_OTP_SUCCESS)
+    }
+  )(response)
+
+}
+
+const resetPasswordHandler = async (payload: ResetPasswordUseCaseParams, getState: typeof useAuthState.getState) => {
+  const {setIsLoading} = getState();
+
+  setIsLoading(true);
+  const response = await resetPasswordUseCase.execute(payload);
+
+  fold<Failure, GeneralResponse, void>(
+    (failure) => {
+      setIsLoading(false);
+      showToast('error', 'Error!', failure.message || messages.ERROR)
+    },
+    () => {
+      setIsLoading(false);
+      router.push("/sign-in")
+      showToast('error', 'Success!', messages.RESET_PASSWORD_SUCCESS)
     }
   )(response)
 
