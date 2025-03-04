7dd6cbb488b35b9410c1b4734d897ef991862e0e
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-04 07:24:30 +0300

    feat(authentication): moved home page to tabs

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import WelcomeScreen from '@/src/feature/authentication/presentation/screens/WelcomeScreen';
+import { useAuthState } from '@/src/feature/authentication/presentation/state/authState';
 
 export default function Index() {
+  const {token} = useAuthState.getState()
+
+  if(token) {
+    return <WelcomeScreen/>
+  }
+
   return <WelcomeScreen/>
 }
