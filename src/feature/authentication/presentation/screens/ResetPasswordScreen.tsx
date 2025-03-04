8d0366c1a391599f36e0c7ebf692b8458cca531f
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-04 20:17:34 +0300

    feat(authentication): link reset password screen to backend

--- src/feature/authentication/presentation/screens/ResetPasswordScreen.tsx
+++ src/feature/authentication/presentation/screens/ResetPasswordScreen.tsx
@@ -1,10 +1,30 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import { router } from 'expo-router';
 import { ArrowLeft } from 'lucide-react-native';
 import Button from '@/src/shared/presentation/form/button';
 import PasswordFieldInput from '@/src/shared/presentation/form/password-input';
+import { useAuthState } from '@/src/feature/authentication/presentation/state/authState';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { resetPasswordSchema } from '@/src/core/validation/auth-validation';
+import { authBloc } from '@/src/feature/authentication/presentation/state/authBloc';
+import { AUTH_EVENTS } from '@/src/feature/authentication/presentation/state/authEvent';
 
 const ResetPasswordScreen = () => {
+  const {isLoading} = useAuthState.getState();
+  const {setValue, handleSubmit, watch, formState: { errors }} = useForm({
+    resolver: zodResolver(resetPasswordSchema),
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "onChange"
+  })
+
+  const onSubmit = async (data: any) => {
+    await authBloc.handleAuthEvent(AUTH_EVENTS.RESET_PASSWORD, { password: data.password, confirmPassword: data.confirmPassword });
+  }
+
   return (
     <SafeAreaView className="bg-primary h-screen w-screen">
       <View className="flex flex-col justify-between items-center h-full w-full">
@@ -21,12 +41,29 @@ const ResetPasswordScreen = () => {
             </View>
           </View>
           <View className="flex flex-col justify-start items-center w-full my-[24px] gap-y-[24px]">
-            <PasswordFieldInput label="New Password" placeholder="Enter new password" value="" onChange={(value) => console.log(value)} />
-            <PasswordFieldInput label="Confirm New Password" placeholder="Renter new password" value="" onChange={(value) => console.log(value)} />
+            <PasswordFieldInput
+              label="New Password"
+              placeholder="Enter new password"
+              value={watch("password")}
+              onChangeText={(text) => setValue("password", text)}
+              error={errors.password?.message}
+            />
+            <PasswordFieldInput
+              label="Confirm New Password"
+              placeholder="Renter new password"
+              value={watch("confirmPassword")}
+              onChangeText={(text) => setValue("confirmPassword", text)}
+              error={errors.confirmPassword?.message}
+            />
           </View>
         </View>
         <View className="w-screen p-[24px] border-t-[1px] border-t-quaternary">
-          <Button onPress={() => router.push('/otp')} label="Continue" type="primary" />
+          <Button
+            onPress={handleSubmit(onSubmit)}
+            label="Continue"
+            type="primary"
+            isLoading={isLoading}
+          />
         </View>
       </View>
     </SafeAreaView>
