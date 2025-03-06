eba77ed9551055ef3b57ca41b0cbdc390d1e7471
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-06 22:46:38 +0300

    feat(expense): add expenses dependency injections

--- src/init_dependencies.ts
+++ src/init_dependencies.ts
@@ -12,6 +12,14 @@ import { InsightsDatasourceImpl } from '@/src/feature/insights/data/datasource/i
 import { InsightRepositoryImpl } from '@/src/feature/insights/data/repositoryImpl/insight-repositoryImp';
 import { GetDailySpendUseCase } from '@/src/feature/insights/domain/use-case/use-get-daily-spend';
 import { GetTopExpensesUseCase } from '@/src/feature/insights/domain/use-case/use-get-top-expenses';
+import { ExpensesService } from '@/src/core/service/expenses';
+import { ExpenseDatasourceImpl } from '@/src/feature/expenses/data/datasource/expense-datasource';
+import { ExpenseRepositoryImpl } from '@/src/feature/expenses/data/repositoryImpl/expense-repositoryImpl';
+import { CreateExpenseUseCase } from '@/src/feature/expenses/domain/use-case/use-create-expense';
+import { DeleteExpenseUseCase } from '@/src/feature/expenses/domain/use-case/use-delete-expense';
+import { GetAllExpenseUseCase } from '@/src/feature/expenses/domain/use-case/use-get-all-expense';
+import { GetExpenseByIdUseCase } from '@/src/feature/expenses/domain/use-case/use-get-expense-by-id';
+import { UpdateExpenseUseCase } from '@/src/feature/expenses/domain/use-case/use-update-expense';
 
 // Authentication Dependencies
 const authenticationService = new AuthenticationService();
@@ -34,3 +42,14 @@ export const getMonthlySpendUseCase = new GetDailySpendUseCase(insightsRepositor
 export const getTopExpensesUseCase = new GetTopExpensesUseCase(insightsRepository);
 export const getTotalSpendUseCase = new GetTopExpensesUseCase(insightsRepository);
 export const getTotalSpendByCategoryUseCase = new GetTopExpensesUseCase(insightsRepository);
+
+
+// Expense Dependencies
+const expensesService = new ExpensesService();
+const expenseDatasource = new ExpenseDatasourceImpl(expensesService);
+const expenseRepository = new ExpenseRepositoryImpl(expenseDatasource);
+export const createExpenseUseCase = new CreateExpenseUseCase(expenseRepository);
+export const deleteExpenseUseCase = new DeleteExpenseUseCase(expenseRepository);
+export const getAllExpenseUseCase = new GetAllExpenseUseCase(expenseRepository);
+export const getExpenseByIdUseCase = new GetExpenseByIdUseCase(expenseRepository);
+export const updateExpenseUseCase = new UpdateExpenseUseCase(expenseRepository);
