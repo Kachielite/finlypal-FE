517bf7fc9aa1f3aa15a83af02912891d1db7b6e2
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-08 15:08:28 +0300

    feat(category): add category entity, model and datasource

---
+++ src/feature/category/data/datasource/category-datasource.ts
@@ -0,0 +1,25 @@
+import { CategoryModel } from '@/src/feature/category/data/model/category-model';
+import { CategoryService } from '@/src/core/service/categories';
+import { Exception } from '@/src/core/error/exception';
+
+export interface CategoryDatasource {
+  getCategories(): Promise<CategoryModel[]>
+}
+
+export class CategoryDatasourceImpl implements CategoryDatasource {
+  constructor(private categoryService: CategoryService) {
+  }
+
+  async getCategories(): Promise<CategoryModel[]> {
+    try {
+      const response = await this.categoryService.getCategories();
+      return CategoryModel.fromJSONList(response)
+    } catch (error: unknown) {
+      if (error && typeof error === "object" && "code" in error && "message" in error) {
+        throw new Exception(error.message as string);
+      } else {
+        throw new Exception("An unknown error occurred");
+      }
+    }
+  }
+}
