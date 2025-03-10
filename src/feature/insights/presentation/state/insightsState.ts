d734ca8ed7dab39147db6b62f579cdb0cc89a605
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-10 04:39:26 +0300

    feat(insights): add insights events

--- src/feature/insights/presentation/state/insightsState.ts
+++ src/feature/insights/presentation/state/insightsState.ts
@@ -2,6 +2,7 @@ import { MonthlySpend } from '@/src/feature/insights/domain/entity/MonthlySpend'
 import { TotalSpendByCategory } from '@/src/feature/insights/domain/entity/TotalSpendByCategory';
 import { TopExpenses } from '@/src/feature/insights/domain/entity/TopExpenses';
 import { create } from 'zustand';
+import { DailySpend } from '@/src/feature/insights/domain/entity/DailySpend';
 
 export interface InsightsState {
   isLoading: boolean;
@@ -10,12 +11,14 @@ export interface InsightsState {
   monthlySpending: MonthlySpend[];
   totalSpendByCategory: TotalSpendByCategory[];
   topExpenses: TopExpenses[];
+  dailySpend: DailySpend[];
   setIsLoading: (isLoading: boolean) => void;
   setTotalIncome: (totalIncome: number) => void;
   setTotalExpense: (totalExpense: number) => void;
   setMonthlySpending: (monthlySpending: MonthlySpend[]) => void;
   setTotalSpendByCategory: (totalSpendByCategory: TotalSpendByCategory[]) => void;
   setTopExpenses: (topExpenses: TopExpenses[]) => void;
+  setDailySpend: (dailySpend: DailySpend[]) => void;
 }
 
 
@@ -26,10 +29,12 @@ export const useInsightsState = create<InsightsState>((set) => ({
   monthlySpending: [],
   totalSpendByCategory: [],
   topExpenses: [],
+  dailySpend: [],
   setIsLoading: (isLoading: boolean) => set({ isLoading }),
   setTotalIncome: (totalIncome: number) => set({ totalIncome }),
   setTotalExpense: (totalExpense: number) => set({ totalExpense }),
   setMonthlySpending: (monthlySpending: MonthlySpend[]) => set({ monthlySpending }),
   setTotalSpendByCategory: (totalSpendByCategory: TotalSpendByCategory[]) => set({ totalSpendByCategory }),
   setTopExpenses: (topExpenses: TopExpenses[]) => set({ topExpenses }),
+  setDailySpend: (dailySpend: DailySpend[]) => set({ dailySpend }),
 }));
