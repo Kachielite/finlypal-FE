4f32bca3d0512b440ac3b6332e7583ef1a4ad218
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-14 21:43:45 +0300

    feat(budget): add delete budget use case

---
+++ src/feature/budget/domain/use-case/use-delete-budget.ts
@@ -0,0 +1,19 @@
+import { UseCase } from '@/src/core/use-case/use-case';
+import { GeneralResponse } from '@/src/shared/domain/entity/general-response';
+import { BudgetRepository } from '@/src/feature/budget/domain/repository/budget-repository';
+import { Either } from 'fp-ts/Either';
+import { Failure } from '@/src/core/error/failure';
+
+export class DeleteBudgetUseCaseParams {
+  constructor(public id: string) {}
+}
+
+export class DeleteBudgetUseCase extends UseCase<GeneralResponse, DeleteBudgetUseCaseParams>{
+  constructor(private budgetRepository: BudgetRepository) {
+    super();
+  }
+
+  async execute(params: DeleteBudgetUseCaseParams): Promise<Either<Failure, GeneralResponse>> {
+    return await this.budgetRepository.deleteBudget(params.id);
+  }
+}
