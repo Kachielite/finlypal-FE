e6fa7a70a3fe226c3f5bfc8266f5428f144e2dc6
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-14 21:40:52 +0300

    feat(budget): add create budget use case

---
+++ src/feature/budget/domain/use-case/use-create-budget.ts
@@ -0,0 +1,30 @@
+import { UseCase } from '@/src/core/use-case/use-case';
+import { Budget } from '@/src/feature/budget/domain/entity/budget';
+import { BudgetRepository } from '@/src/feature/budget/domain/repository/budget-repository';
+import { Either } from 'fp-ts/Either';
+import { Failure } from '@/src/core/error/failure';
+
+export class CreateBudgetUseCaseParams {
+  constructor(
+    public budgetName: string,
+    public startDate: string,
+    public endDate: string,
+    public totalBudget: number
+  ) {
+  }
+}
+
+export class CreateBudgetUseCase extends UseCase<Budget, CreateBudgetUseCaseParams>{
+  constructor(private budgetRepository: BudgetRepository) {
+    super();
+  }
+
+  async execute(params: CreateBudgetUseCaseParams): Promise<Either<Failure, Budget>> {
+    return await this.budgetRepository.createBudget({
+      budgetName: params.budgetName,
+      startDate: params.startDate,
+      endDate: params.endDate,
+      totalBudget: params.totalBudget
+    })
+  }
+}
