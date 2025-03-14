7508c62c023f455985a343e47aea98442cfe6feb
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-14 20:45:09 +0300

    feat(budget): add budget repository

---
+++ src/feature/budget/domain/repository/budget-repository.ts
@@ -0,0 +1,12 @@
+import { Either } from 'fp-ts/Either';
+import { Failure } from '@/src/core/error/failure';
+import { Budget } from '@/src/feature/budget/domain/entity/budget';
+import { GeneralResponse } from '@/src/shared/domain/entity/general-response';
+
+export interface BudgetRepository {
+  getBudget(budgetId: string): Promise<Either<Failure, Budget>>;
+  getAllBudgets(page?: number, pageSize?: number): Promise<Either<Failure, Budget[]>>;
+  createBudget({budgetName, startDate, endDate, totalBudget}: {budgetName: string, startDate: string, endDate: string, totalBudget: number}): Promise<Either<Failure, Budget>>;
+  updateBudget({budgetId, budgetName, startDate, endDate, totalBudget}: {budgetId: number, budgetName: string, startDate: string, endDate: string, totalBudget: number}): Promise<Either<Failure, Budget>>;
+  deleteBudget(budgetId: string): Promise<GeneralResponse>;
+}
