cb4566d0f995cca20e7b22521bebceed08ee556b
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-16 23:19:08 +0300

    feat(budget-item): add budget item bloc

--- src/core/constants/messages.ts
+++ src/core/constants/messages.ts
@@ -15,6 +15,17 @@ const UPDATE_BUDGET_SUCCESS = "Planning updated successfully";
 const UPDATE_BUDGET_FAILED = "Planning update failed";
 const DELETE_BUDGET_SUCCESS = "Planning deleted successfully";
 const DELETE_BUDGET_FAILED = "Planning deletion failed";
+// Budget Items
+const CREATE_BUDGET_ITEMS_SUCCESS = "Budget Items created successfully";
+const CREATE_BUDGET_ITEMS_FAILED = "Budget Items creation failed";
+const UPDATE_BUDGET_ITEMS_SUCCESS = "Budget Items updated successfully";
+const UPDATE_BUDGET_ITEMS_FAILED = "Budget Items update failed";
+const DELETE_BUDGET_ITEMS_SUCCESS = "Budget Items deleted successfully";
+const DELETE_BUDGET_ITEMS_FAILED = "Budget Items deletion failed";
+const GET_BUDGET_ITEMS_SUCCESS = "Budget Items fetched successfully";
+const GET_BUDGET_ITEMS_FAILED = "Budget Items fetch failed";
+const GET_BUDGET_ITEM_BY_ID_SUCCESS = "Budget Item fetched successfully";
+const GET_BUDGET_ITEM_BY_ID_FAILED = "Budget Item fetch failed";
 
 export default {
     ERROR,
@@ -32,5 +43,15 @@ export default {
     UPDATE_BUDGET_SUCCESS,
     UPDATE_BUDGET_FAILED,
     DELETE_BUDGET_SUCCESS,
-    DELETE_BUDGET_FAILED
+    DELETE_BUDGET_FAILED,
+    CREATE_BUDGET_ITEMS_SUCCESS,
+    CREATE_BUDGET_ITEMS_FAILED,
+    UPDATE_BUDGET_ITEMS_SUCCESS,
+    UPDATE_BUDGET_ITEMS_FAILED,
+    DELETE_BUDGET_ITEMS_SUCCESS,
+    DELETE_BUDGET_ITEMS_FAILED,
+    GET_BUDGET_ITEMS_SUCCESS,
+    GET_BUDGET_ITEMS_FAILED,
+    GET_BUDGET_ITEM_BY_ID_SUCCESS,
+    GET_BUDGET_ITEM_BY_ID_FAILED
 }
