4611df6a32163f290095ffd6c0b565aba7de0e9d
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-22 08:15:39 +0300

    feat(savings): add savings route

---
+++ app/savings/_layout.tsx
@@ -0,0 +1,17 @@
+import { useAuthState } from '@/src/feature/authentication/presentation/state/authState';
+import { Redirect, Stack } from 'expo-router';
+import React from 'react';
+
+export default function SavingsLayout() {
+  const {token} = useAuthState.getState()
+
+  if(!token) {
+    return <Redirect href="/authentication/welcome"/>
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
