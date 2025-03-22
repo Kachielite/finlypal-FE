9c0e8c8ae5cdcbbcd1d88e81e621a661f1adeac1
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-22 07:56:28 +0300

    feat(savings): add savings implementation

---
+++ src/feature/savings/domain/repository/savings-repository.ts
@@ -0,0 +1,13 @@
+import { Either } from 'fp-ts/Either';
+import { Failure } from '@/src/core/error/failure';
+import { Savings } from '@/src/feature/savings/domain/entity/savings';
+import { SavingsSchema } from '@/src/core/validation/savings-validation';
+import { GeneralResponse } from '@/src/shared/domain/entity/general-response';
+
+export interface SavingsRepository {
+  getSavings(page: number, pageSize: number): Promise<Either<Failure, Savings[]>>;
+  getSavingsById(savingsId: number): Promise<Either<Failure, Savings>>;
+  createSavings(data: typeof SavingsSchema): Promise<Either<Failure, Savings>>;
+  updateSavings(data: typeof SavingsSchema, savingsId: number): Promise<Either<Failure, Savings>>;
+  deleteSavings(savingsId: number): Promise<Either<Failure, GeneralResponse>>;
+}
