4791a5e37ded6107c6f0d0e62789dc18198c5fd6
Kachielite <40024356+Kachielite@users.noreply.github.com>
2025-03-22 07:30:07 +0300

    feat(savings): add savings modal

---
+++ src/feature/savings/data/model/savings-modal.ts
@@ -0,0 +1,39 @@
+import { Savings, SavingsStatus } from '@/src/feature/savings/domain/entity/savings';
+
+export class SavingsModal extends Savings {
+  constructor(
+    public id: number,
+    public icon: string,
+    public status: SavingsStatus,
+    public statusTooltip: string,
+    public expenses: any[],
+    public goalName: string,
+    public targetAmount: number,
+    public savedAmount: number,
+    public startDate: string,
+    public endDate: string,
+    public createdAt: string
+  ) {
+    super(id, icon, status, statusTooltip, expenses, goalName, targetAmount, savedAmount, startDate, endDate, createdAt);
+  }
+
+  static fromListJson(jsonList: any[]): SavingsModal[] {
+    return jsonList?.map(SavingsModal.fromJson);
+  }
+
+  static fromJson(json: any): SavingsModal {
+    return new SavingsModal(
+      json.id,
+      json.icon,
+      json.status,
+      json.status_tooltip,
+      json.expenses,
+      json.goal_name,
+      json.target_amount,
+      json.saved_amount,
+      json.start_date,
+      json.end_date,
+      json.created_at
+    );
+  }
+}
